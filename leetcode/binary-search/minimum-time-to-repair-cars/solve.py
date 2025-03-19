da7ce9407eb4bfc376570ffa20fff68782c816e7
Thiago Barros <122405319+thiago-abarros@users.noreply.github.com>
2025-03-19 07:59:15 -0300

    feat: added solve to minimum time to repair cars

---
+++ leetcode/binary-search/minimum-time-to-repair-cars/solve.py
@@ -0,0 +1,8 @@
+from typing import List 
+
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        
+
+        def _calculate_repair_time(self, rank, n_cars):
+            return rank * (n_cars**2)
